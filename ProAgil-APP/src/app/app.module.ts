5e9cbce9a2e2d422389ae6131524ccad8001d975
breno_braguim <bre_ab@hotmail.com>
2021-04-02 12:46:39 -0300

    Incluindo Nav e Tabela

--- ProAgil-APP/src/app/app.module.ts
+++ ProAgil-APP/src/app/app.module.ts
@@ -5,11 +5,13 @@ import{HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
-      EventosComponent
+      EventosComponent,
+      NavComponent
    ],
   imports: [
     BrowserModule,
